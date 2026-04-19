23211d9522492f0449eef3640c67ace2aa932617
simrankaurrani262-crypto <simrankaurrani262@gmail.com>
2026-04-19 11:47:03 +0530

    Update __init__.py

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,2 +1,30 @@
-from .keyboards import *
-from .formatters import *
+# Utils package - Add exports for new modules
+try:
+    from utils.visualizations import (
+        generate_army_chart, generate_resource_chart, generate_battle_analytics,
+        generate_building_progress, generate_leaderboard_chart, generate_power_radar,
+        generate_economy_chart, generate_activity_heatmap, generate_comparison_chart,
+        generate_achievement_progress, generate_quick_status, cleanup_old_charts
+    )
+    VISUALIZATIONS_AVAILABLE = True
+except ImportError:
+    VISUALIZATIONS_AVAILABLE = False
+
+try:
+    from utils.animations import (
+        generate_battle_animation, generate_levelup_animation, generate_reward_animation,
+        generate_spin_animation, generate_training_animation, generate_achievement_animation,
+        generate_attack_alert_animation, generate_login_streak_animation, cleanup_old_animations
+    )
+    ANIMATIONS_AVAILABLE = True
+except ImportError:
+    ANIMATIONS_AVAILABLE = False
+
+try:
+    from utils.image_renderer import (
+        render_hero_card, render_battle_report, render_kingdom_banner,
+        render_spy_report, render_notification_card, render_leaderboard_podium
+    )
+    IMAGE_RENDERER_AVAILABLE = True
+except ImportError:
+    IMAGE_RENDERER_AVAILABLE = False
